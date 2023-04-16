692abbcb78c407b2a44ea7fea04bb10db5a9b0cb
kdbswo <kdbswo@gmail.com>
2023-04-16 15:21:12 +0900

    new counter app

---
+++ app/src/main/java/com/loci/counter/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.loci.counter
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val countBtn = findViewById<Button>(R.id.countBtn)
+        val countText = findViewById<TextView>(R.id.countText)
+        var count = 0
+
+        countText.text = count.toString()
+
+        countBtn.setOnClickListener {
+            count += 1
+            countText.text = count.toString()
+        }
+    }
+}

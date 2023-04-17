38b0404eae42d14a8d34ef416e8f5500a20c994c
kdbswo <kdbswo@gmail.com>
2023-04-17 15:32:28 +0900

    new counter app

--- app/src/main/java/com/loci/counter/MainActivity.kt
+++ app/src/main/java/com/loci/counter/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         var count = 0
 
         countText.text = count.toString()
-
+        //test1
         countBtn.setOnClickListener {
             count += 1
             countText.text = count.toString()

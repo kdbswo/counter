4a89f9a77de93f75164a7caaed69731976286a7b
kdbswo <kdbswo@gmail.com>
2023-04-20 18:43:02 +0900

    set sharedPreference(save count)

--- app/src/main/java/com/loci/counter/MainActivity.kt
+++ app/src/main/java/com/loci/counter/MainActivity.kt
@@ -14,12 +14,18 @@ class MainActivity : AppCompatActivity() {
         val minusBtn = findViewById<Button>(R.id.minusBtn)
         val countText = findViewById<TextView>(R.id.countText)
         val resetBtn = findViewById<TextView>(R.id.resetBtn)
-        var count = 0
 
-        countText.text = count.toString()
+        val sharedPreference = getSharedPreferences("number", 0)
+        val editor = sharedPreference.edit()
+        val saveNumber = sharedPreference.getInt("count", 0).toString()
+        countText.text = saveNumber
+        var count = saveNumber.toInt()
+
         countBtn.setOnClickListener {
             count += 1
             countText.text = count.toString()
+            editor.putInt("count", count)
+            editor.apply()
         }
 
         minusBtn.setOnClickListener {
